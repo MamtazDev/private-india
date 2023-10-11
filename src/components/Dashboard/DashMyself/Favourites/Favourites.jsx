f08e5b61332678969adb13d7b48750472dc28fb7
tonni-akter-dev <tanniakhter123@gmail.com>
2023-10-11 10:11:57 +0600

    favourites page done

--- src/components/Dashboard/DashMyself/Favourites/Favourites.jsx
+++ src/components/Dashboard/DashMyself/Favourites/Favourites.jsx
@@ -7,8 +7,34 @@ const Favourites = () => {
         <h3>Favourties</h3>
         <p>Manage and customize your favourite songs.</p>
         <input type="text" placeholder="Search favouries" />
-        <button className="btn btn-outline-dark m-3">Search</button>
+        <button className="btn btn-outline-dark m-3 btn_lg">Search</button>
       </div>
+      <div className="fav_header">
+        <p>Add names to your favourites</p>
+        <input type="text" className="name_input" placeholder="Name" />
+        <button className="btn btn-outline-dark m-3 btn_lg">Add</button>
+      </div>
+      {/* track div */}
+      {[ 1, 2, 3].map((item) => (
+       <div className="d-flex justify-content-between gap-4 tracks">
+         <div className="track_parent">
+          <div className="track_container">
+            <p>Track {item}</p>
+            <button className="">
+              <i class="fa-solid fa-trash"></i>
+            </button>
+          </div>
+        </div>
+        <div className="track_parent">
+          <div className="track_container">
+            <p>Track {item}</p>
+            <button className="">
+              <i class="fa-solid fa-trash"></i>
+            </button>
+          </div>
+        </div>
+       </div>
+      ))}
     </div>
   );
 };
