fa1d7b62a1a6cc6b50ce82c303d1808c3419c478
tonni-akter-dev <tanniakhter123@gmail.com>
2023-10-14 12:22:12 +0600

    update

--- src/components/Dashboard/DashMyself/SavedQueues/SavedQueues.jsx
+++ src/components/Dashboard/DashMyself/SavedQueues/SavedQueues.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import "./SavedQueues.css";
-import squareup from '../../../../assets/squareup.png'
-import squaredown from '../../../../assets/sqd.png'
+import squareup from "../../../../assets/squareup.png";
+import squaredown from "../../../../assets/sqd.png";
 const SavedQueues = () => {
   return (
     <>
       <div className="favourites_container mb-5 ">
         <div className="fav_header">
-          <h3>Saved Queues</h3>
+          <h3>
+            Saved <span>Queues</span>
+          </h3>
           <p>Manage and customize your Saved Queues.</p>
           <input type="text" placeholder="Search playlists" />
-          <button className="btn btn-outline-dark m-3 btn_lg">Search</button>
+          <button className="btn btn_outline_dark m-3 btn_lg">
+            <a href="">Search</a>
+          </button>
         </div>
         <div className="ques_container">
           <div>
-            <p className="ques_header">Your Saved Queues</p>
+            <p className="ques_header">
+              Your Saved <span>Queues</span>
+            </p>
             {[1, 2, 3, 4, 5, 6].map((item) => (
               <div className="queues_parent">
                 <div className="track_container">
@@ -24,7 +30,6 @@ const SavedQueues = () => {
                   </div>
 
                   <button className="d-flex gap-3">
-             
                     <i class="fa-solid fa-wrench"></i>
                     <i class="fa-solid fa-trash"></i>
                   </button>
@@ -33,7 +38,9 @@ const SavedQueues = () => {
             ))}
           </div>
           <div>
-            <p className="ques_header">Tracks In Saved Queue</p>
+            <p className="ques_header">
+              Tracks In <span>Saved Queue</span>
+            </p>
             {[1, 2, 3, 4, 5, 6].map((item) => (
               <div className="queues_parent">
                 <div className="track_container">
@@ -43,8 +50,8 @@ const SavedQueues = () => {
                   </div>
 
                   <button className="d-flex gap-3">
-                  <img src={squareup} alt="" />
-                  <img src={squaredown} alt="" />
+                    <img src={squareup} alt="" />
+                    <img src={squaredown} alt="" />
                     <i class="fa-solid fa-trash"></i>
                   </button>
                 </div>
@@ -52,7 +59,9 @@ const SavedQueues = () => {
             ))}
           </div>
         </div>
-        <button className="btn btn-dark m-3 btn_lg">Save Changes</button>
+        <button className="btn btn_outline_dark m-3 btn_lg">
+          <a href="">Save Changes</a>
+        </button>
       </div>
     </>
   );
