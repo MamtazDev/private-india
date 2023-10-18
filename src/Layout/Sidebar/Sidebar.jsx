68d8fa3a2efb6aaeb5a92ead1dbb23f0b6f54e13
tonni-akter-dev <tanniakhter123@gmail.com>
2023-10-18 16:24:11 +0600

    sidebar 1 update

--- src/Layout/Sidebar/Sidebar.jsx
+++ src/Layout/Sidebar/Sidebar.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import "./Sidebar.css";
-import profile from "../../assets/g7.jpg";
+import profile from "../../assets/profile_cartoon.jpg";
 import { Link, NavLink } from "react-router-dom";
-
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
 const Sidebar = () => {
   const [sideCollapse, setSideCollapse] = useState(false);
   const [smallSideCollapse, setSmallSideCollapse] = useState(false);
@@ -18,43 +19,66 @@ const Sidebar = () => {
             sideCollapse ? "navCollapse" : "nav_notCollapse"
           }`}
         >
-          <div className="logo">
-            <div
-              onClick={(e) => setSideCollapse(!sideCollapse)}
-              className={` ${
-                sideCollapse ? "collapse_largeDeviceIcon" : "largeDeviceIcon"
-              }`}
-            >
-              {sideCollapse ? (
-                <i class="fa-solid fa-bars"></i>
-              ) : (
-                <i class="fa-solid fa-xmark"></i>
-              )}
-            </div>
-            <div
-              onClick={(e) => setSmallSideCollapse(!smallSideCollapse)}
-              className={`${
-                smallSideCollapse ? "smallDeviceIcon" : "collapse_smalll"
-              }`}
-            >
-              {smallSideCollapse ? (
-                <i class="fa-solid fa-xmark"></i>
-              ) : (
-                <i class="fa-solid fa-bars"></i>
-              )}
-            </div>
-          </div>
           <div
             className={` ${
               sideCollapse ? "sidebar_collapse_profile" : "sidebar_profile"
             }`}
           >
-            <div>
-              <img src={profile} alt="" />
+            {!sideCollapse && (
+              <>
+                <div>
+                  <img src={profile} alt="" />
+                </div>
+                <div>
+                  <small>MUSICIAN</small>
+                  <br />
+                </div>
+              </>
+            )}
+            <div className="logo">
+              <div
+                onClick={(e) => setSideCollapse(!sideCollapse)}
+                className={` ${
+                  sideCollapse ? "collapse_largeDeviceIcon" : "largeDeviceIcon"
+                }`}
+              >
+                {sideCollapse ? (
+                  // <i class="fa-solid fa-bars"></i>
+                  <IoIosArrowForward />
+                ) : (
+                  <IoIosArrowBack />
+                )}
+              </div>
+              <div
+                onClick={(e) => setSmallSideCollapse(!smallSideCollapse)}
+                className={`${
+                  smallSideCollapse ? "smallDeviceIcon" : "collapse_smalll"
+                }`}
+              >
+                {smallSideCollapse ? (
+                  <i class="fa-solid fa-xmark"></i>
+                ) : (
+                  <i class="fa-solid fa-bars"></i>
+                )}
+              </div>
             </div>
-            <p>Username</p>
-            <hr />
           </div>
+
+          {/* <div className="hr_design" /> */}
+          {sideCollapse && (
+            <>
+              <div className="sidebar_profile mt-3 mb-2">
+                <img src={profile} alt="" />
+              </div>
+              <div>
+                <small>MUSICIAN</small>
+                <br />
+              </div>
+            </>
+          )}
+
+          <div className="hr_design" />
+
           <NavLink
             to="/dashboard/overview"
             className={`nav-option option1 ${isActive ? "active" : ""} ${
